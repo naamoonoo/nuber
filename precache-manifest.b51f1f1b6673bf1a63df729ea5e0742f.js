b40cf1331ee927e13afd44afcd42715fb2fd3a06
naamoonoo <naamoonoo.y@gmail.com>
2020-01-25 00:31:36 +0100

    Updates

---
+++ precache-manifest.b51f1f1b6673bf1a63df729ea5e0742f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40ce0f829d0f46d1b898be9ef2a6fde5",
+    "url": "/nuber_client/index.html"
+  },
+  {
+    "revision": "1a9883354d44f0fda23a",
+    "url": "/nuber_client/static/css/2.c829125e.chunk.css"
+  },
+  {
+    "revision": "1a9883354d44f0fda23a",
+    "url": "/nuber_client/static/js/2.ab2d12f9.chunk.js"
+  },
+  {
+    "revision": "335932bcfe373407d7c8acc8ad15eddd",
+    "url": "/nuber_client/static/js/2.ab2d12f9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f59fd455b64b4a60450e",
+    "url": "/nuber_client/static/js/main.9ac1f9d4.chunk.js"
+  },
+  {
+    "revision": "238fdb81c93d6078164c",
+    "url": "/nuber_client/static/js/runtime-main.febbeefc.js"
+  },
+  {
+    "revision": "ce98de714d64a64ea0c5cfe3b156e1c0",
+    "url": "/nuber_client/static/media/add.ce98de71.svg"
+  },
+  {
+    "revision": "e39814da20d3196b77a3ae6d1b7253f5",
+    "url": "/nuber_client/static/media/addEmpty.e39814da.svg"
+  },
+  {
+    "revision": "0c50775d6bbe04f4375afea9a1f95eb2",
+    "url": "/nuber_client/static/media/backArrow.0c50775d.svg"
+  },
+  {
+    "revision": "f1662d11088621fd43b5f95d795d749e",
+    "url": "/nuber_client/static/media/bg.f1662d11.png"
+  },
+  {
+    "revision": "47e28e6e1273b6a4768e9485111214b9",
+    "url": "/nuber_client/static/media/chat.47e28e6e.svg"
+  },
+  {
+    "revision": "616417cd4111d2b6702a17181afa2ad9",
+    "url": "/nuber_client/static/media/facebook.616417cd.svg"
+  },
+  {
+    "revision": "5d1bbb3376bed5b9a2cec176b70f4b4c",
+    "url": "/nuber_client/static/media/loading.5d1bbb33.svg"
+  },
+  {
+    "revision": "00b27c6bbcdc5e3c924b155ef99e5b23",
+    "url": "/nuber_client/static/media/menu.00b27c6b.svg"
+  },
+  {
+    "revision": "d966831bcebb850bdee67469377ec765",
+    "url": "/nuber_client/static/media/nextArrow.d966831b.svg"
+  }
+]);
