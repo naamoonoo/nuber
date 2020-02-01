f6c4fec5b4d14de21bf50a5a30305ec188323bbe
naamoonoo <naamoonoo.y@gmail.com>
2020-02-01 18:02:39 +0100

    [#14]chat refactored and email verification

--- client/src/Components/App/AppPresenter.tsx
+++ client/src/Components/App/AppPresenter.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
-import Routes from "../../Routes";
 import AddPlace from "../../Routes/AddPlace";
 import Chat from "../../Routes/Chat";
 import EditAccount from "../../Routes/EditAccount";
+import EmailVerify from "../../Routes/EmailVerify";
 import FindAddress from "../../Routes/FindAddress";
 import GetPlaces from "../../Routes/GetPlaces";
 import Home from "../../Routes/Home";
@@ -11,6 +11,7 @@ import Login from "../../Routes/Login";
 import PhoneLogin from "../../Routes/PhoneLogin";
 import Ride from "../../Routes/Ride";
 import RideHistory from "../../Routes/RideHistory";
+import Routes from "../../Routes/routes";
 import Settings from "../../Routes/Settings";
 import SignUp from "../../Routes/SignUp";
 import SocialLogin from "../../Routes/SocialLogin";
@@ -41,16 +42,18 @@ const LoggedOutRoutes: React.FC = () => (
 
 const LoggedInRoutes: React.FC = () => (
 	<Switch>
-		<Route path={Routes.HOME} exact={true} component={Home} />
+		<Route path={Routes.NUBER} exact={true} component={Home} />
 		<Route path={Routes.RIDE_FORM} component={Ride} />
 		<Route path={Routes.EDIT_ACCOUNT} component={EditAccount} />
+		<Route path={Routes.EMAIL_VERIFY_FORM} component={EmailVerify} />
+		<Route path={Routes.EMAIL_VERIFY} component={EmailVerify} />
 		<Route path={Routes.SETTING} component={Settings} />
 		<Route path={Routes.PLACES} component={GetPlaces} />
 		<Route path={Routes.ADD_PLACE} component={AddPlace} />
 		<Route path={Routes.FIND_ADDRESS} component={FindAddress} />
 		<Route path={Routes.CHAT_FORM} component={Chat} />
 		<Route path={Routes.RIDE_HISTORY} component={RideHistory} />
-		<Redirect path={"*"} to={Routes.HOME} />
+		<Redirect path={"*"} to={Routes.NUBER} />
 	</Switch>
 );
 
